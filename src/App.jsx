284a6a496d84ee4e49c655928171ab794db11f80
hossames <100873739+hossames@users.noreply.github.com>
2026-05-18 04:47:32 +0300

    helo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
-import MkS from "./mks";
+import MKS from "./mks";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mks" element={<MkS />} />
+        <Route path="/mks" element={<MKS />} />
       </Routes>
     </>
   );
